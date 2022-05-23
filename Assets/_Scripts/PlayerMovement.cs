fae111550731104c861b11fcdf52ed73f3b1e255
MuhammetInce <muhammetinceee@gmail.com>
2022-05-23 16:44:33 +0300

    + Add : Player Movement Buttons Mech Created but not Working Clearly

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -19,23 +19,29 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Button rightButton;
     [SerializeField] private Button upButton;
     [SerializeField] private Button downButton;
-    [Space]
 
-    [Header("Minimum Coordinate For Buttons")] 
-    [SerializeField] private float minimumY;
-    [SerializeField] private float maximumY;
-    [SerializeField] private float minimumX;
-    [SerializeField] private float maximumX;
-    
+    [Space] [Header("Corner Objects")]
+    [SerializeField] private GameObject leftUpCorner;
+    [SerializeField] private GameObject rightBottomCorner;
 
+    private Vector3 Pos
+    {
+        get => transform.position;
+        set => transform.position = value;
+    }
 
+    private void Start() => StartInit();
     private void Update() => UpdateInit();
 
+    private void StartInit()
+    {
+        ButtonsActivator();
+    }
     private void UpdateInit()
     {
         ButtonChecker();
     }
-
+    
     #region Movement Button
 
     private void ButtonChecker()
@@ -91,12 +97,51 @@ public class PlayerMovement : MonoBehaviour
 
     private void ButtonsActivator()
     {
-        leftButton.gameObject.SetActive(true);
-        rightButton.gameObject.SetActive(true);
-        upButton.gameObject.SetActive(true);
-        downButton.gameObject.SetActive(true);
+        if (Pos.x <= leftUpCorner.transform.position.x && Pos.y >= leftUpCorner.transform.position.y)
+        {
+            rightButton.gameObject.SetActive(true);
+            downButton.gameObject.SetActive(true);
+            print("third");
+        }
+        
+        else if (Pos.x <= leftUpCorner.transform.position.x)
+        {
+            rightButton.gameObject.SetActive(true);
+            upButton.gameObject.SetActive(true);
+            downButton.gameObject.SetActive(true);
+            print("first");
+        }
+
+        else if (Pos.y >= leftUpCorner.transform.position.y)
+        {
+            rightButton.gameObject.SetActive(true);
+            leftButton.gameObject.SetActive(true);
+            downButton.gameObject.SetActive(true);
+            print("second");
+        }
+        
+        if (Pos.x >= rightBottomCorner.transform.position.x && Pos.y <= rightBottomCorner.transform.position.y)
+        {
+            upButton.gameObject.SetActive(true);
+            leftButton.gameObject.SetActive(true);
+            print("last");
+        }
+        
+        else if (Pos.x >= rightBottomCorner.transform.position.x)
+        {
+            upButton.gameObject.SetActive(true);
+            downButton.gameObject.SetActive(true);
+            leftButton.gameObject.SetActive(true);
+            print("fourth");
+        }
+
+        else if (Pos.y <= rightBottomCorner.transform.position.y)
+        {
+            upButton.gameObject.SetActive(true);
+            rightButton.gameObject.SetActive(true);
+            leftButton.gameObject.SetActive(true);
+            print("fifth");
+        }
     }
-
-
     #endregion
 }
